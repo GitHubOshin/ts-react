f03c72bd07c9290e70af3957747cf58ca606a91f
OSHIN <izkp44@gmail.com>
2023-05-18 17:02:34 +0700

    Create TaskList component

---
+++ src/components/TaskList.tsx
@@ -0,0 +1,13 @@
+function TaskList(): JSX.Element {
+  return (
+    <>
+      <h1>All the things that I need todo...</h1>
+      <ul>
+        <li>Task 1</li>
+        <li>Task 2</li>
+      </ul>
+    </>
+  )
+}
+
+export default TaskList
