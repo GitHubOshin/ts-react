83db5e74d53cbb89ddcfeffd142e44e0d60a2180
OSHIN <izkp44@gmail.com>
2023-05-19 07:37:08 +0700

    useRef

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 function TaskForm(): JSX.Element {
+  const inputRef = useRef<HTMLInputElement>(null)
   function saveTask(e: React.FormEvent) {
     e.preventDefault()
-    console.log('Saved the task!!')
+    console.log(inputRef)
   }
   return (
     <form onSubmit={saveTask}>
-      <input type="text" placeholder="Type your task..." />
+      <input type="text" placeholder="Type your task..." ref={inputRef} />
       <button type="submit">save</button>
     </form>
   )
