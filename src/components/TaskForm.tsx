b54c6f5aa3d34dbf47afbd121e9ab9b49bd899d0
OSHIN <izkp44@gmail.com>
2023-05-19 07:25:01 +0700

    Submit Event

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
 function TaskForm(): JSX.Element {
+  function saveTask(e: React.FormEvent) {
+    e.preventDefault()
+    console.log('Saved the task!!')
+  }
   return (
-    <form>
+    <form onSubmit={saveTask}>
       <input type="text" placeholder="Type your task..." />
       <button type="submit">save</button>
     </form>
