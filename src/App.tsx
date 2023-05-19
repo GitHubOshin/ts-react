ebe6d2828cdc90fb5a8a976dbacf352914e2f871
OSHIN <izkp44@gmail.com>
2023-05-19 07:19:33 +0700

    Import TaskForm

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import './App.css'
 import TaskList from './components/TaskList'
 import Item from './models/item'
+import TaskForm from './components/TaskForm'
 
 function App() {
   const [items, setItems] = useState<Item[]>([])
   return (
     <>
+      <TaskForm />
       <TaskList items={items} />
     </>
   )
